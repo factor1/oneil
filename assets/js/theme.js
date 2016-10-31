41680aae6b9ec59ddf5270115e2e982edd60d599
Eric Stout <ericwstout@gmail.com>
2016-10-31 14:07:34 -0700

    Adds header dropdowns

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,4 +1,30 @@
 
+var desktopNavigation = function(){
+  // on click of nav item
+  $('.menu-item-has-children > a').on('click', function(e){
+    e.preventDefault();
+
+    $(this).toggleClass('parent-active');
+
+    // establish "this" and other variables
+    $this = $(this).parent();
+    $dropdown = $this.find('> ul');
+
+    // slide down main dropdown
+    $dropdown.slideToggle(250);
+
+  });
+
+  // click outside the dropdown to close
+  $(document).click(function(event){
+    if(!$(event.target).closest('.menu-item-has-children').length) {
+      $('.sub-menu').slideUp(250);
+      $('.parent-active').removeClass('parent-active');
+    }
+  });
+
+};
+
 jQuery( document ).ready(function( $ ) {
   // Inside of this function, $() will work as an alias for jQuery()
   // and other libraries also using $ will not be accessible under this shortcut
@@ -19,5 +45,8 @@ jQuery( document ).ready(function( $ ) {
   $('#nifty-nav-toggle').on('click', function(){
     $('body').toggleClass('nifty-locked');
   });
+
+  // fire desktop navigation
+  desktopNavigation();
 
 });
