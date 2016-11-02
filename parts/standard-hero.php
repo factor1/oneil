b250ccbc8a772ee309e1fae22711f8605d2e3010
Eric Stout <ericwstout@gmail.com>
2016-11-02 15:25:57 -0700

    Adds standard hero and about template

---
+++ parts/standard-hero.php
@@ -0,0 +1,3 @@
+<?php // Standard Hero ?>
+
+<section class="standard-hero" style="background: url(<?php featuredURL('standard-hero');?>) center center no-repeat;"></section>
---
+++ templates/about-us.php
@@ -0,0 +1,7 @@
+<?php // Template Name: About Us
+
+get_header();
+
+get_template_part('parts/standard-hero');
+
+get_footer();
