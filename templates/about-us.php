0a6f2a97175a24710b5a72558d75995caf5525aa
Eric Stout <ericwstout@gmail.com>
2016-11-04 11:10:49 -0700

    Adds content with button component

--- templates/about-us.php
+++ templates/about-us.php
@@ -12,6 +12,6 @@ get_template_part('parts/icon-content-button');
 
 get_template_part('parts/fifty-fifty');
 
-get_template_part('parts/content-buttons.php');
+get_template_part('parts/content-buttons');
 
 get_footer();
