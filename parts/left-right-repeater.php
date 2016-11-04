615bbb9aca74e19829d4458adabe8eb0760bcdd4
Eric Stout <ericwstout@gmail.com>
2016-11-04 11:31:40 -0700

    Adds solutions template and left right repeater

---
+++ parts/left-right-repeater.php
@@ -0,0 +1,7 @@
+<?php // Left / Right Repeater ?>
+
+<section class="container part--left-right-repeater">
+  <div class="row">
+    
+  </div>
+</section>
---
+++ templates/solutions.php
@@ -0,0 +1,13 @@
+<?php // Template Name: Solutions
+
+get_header();
+
+get_template_part('parts/standard-hero');
+
+get_template_part('parts/breadcrumbs');
+
+get_template_part('parts/intro-content');
+
+get_template_part('parts/left-right-repeater');
+
+get_footer();
