60df6e332a0442c135adf2c8a58e256f613a7e66
Eric Stout <ericwstout@gmail.com>
2016-11-14 14:59:45 -0700

    Adds contact form section

---
+++ templates/contact.php
@@ -0,0 +1,12 @@
+<?php /* Template Name: Contact Us */
+get_header();
+
+get_template_part('parts/standard-hero');
+
+get_template_part('parts/breadcrumbs');
+
+get_template_part('parts/intro-content');
+
+get_template_part('parts/contact-form');
+
+get_footer();
