96697f990c2fa5c2789eebd2970ae0a172ba245b
Eric Stout <ericwstout@gmail.com>
2016-11-14 15:40:04 -0700

    Adds form embed styles

---
+++ parts/form-embed.php
@@ -0,0 +1,9 @@
+<?php // Form Embed ?>
+
+<section class="container contact--form-container">
+  <div class="row">
+    <div class="col-6 col-centered contact--form">
+      <?php the_field('form_embed'); ?>
+    </div>
+  </div>
+</section>
