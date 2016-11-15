2ba709cf9b8d3e900c87fe47bd02b530b9d2d0f7
Eric Stout <ericwstout@gmail.com>
2016-11-14 17:12:10 -0700

    Adds single our work template and template parts

---
+++ single-our_work.php
@@ -0,0 +1,12 @@
+<?php
+get_header();
+
+get_template_part('parts/standard-hero');
+
+get_template_part('parts/breadcrumbs');
+
+get_template_part('parts/intro-content');
+
+get_template_part('parts/our-work-slider');
+
+get_footer();
