e7b7344326c860825de0d9aa37f2bb6854ec4f18
Eric Stout <ericwstout@gmail.com>
2016-11-18 09:39:33 -0700

    Adds small col classes for breadcrumbs

--- parts/breadcrumbs.php
+++ parts/breadcrumbs.php
@@ -2,7 +2,7 @@
 
 <div class="container">
   <div class="row">
-    <div class="col-8">
+    <div class="sm-col-8 col-8">
       <?php
       if ( function_exists('yoast_breadcrumb') ) {
         yoast_breadcrumb('
@@ -11,7 +11,7 @@
       }
       ?>
     </div>
-    <div class="col-4 text-right" style="padding-top: 16px;">
+    <div class="sm-col-4 col-4 text-right" style="padding-top: 16px;">
       <share-button></share-button>
     </div>
   </div>
