c88d06cc10cfdf451127acbfcd901e64e2ea4c87
Eric Stout <ericwstout@gmail.com>
2016-11-18 09:20:41 -0700

    Hides standard hero on single posts without featured img

--- parts/standard-hero.php
+++ parts/standard-hero.php
@@ -1,3 +1,10 @@
-<?php // Standard Hero ?>
+<?php // Standard Hero
 
-<section class="standard-hero" style="background: url(<?php featuredURL('standard-hero');?>) center center no-repeat;"></section>
+if( is_singular() && !has_post_thumbnail() ):
+  // ssshhh
+else: ?>
+
+  <section class="standard-hero" style="background: url(<?php featuredURL('standard-hero');?>) center center no-repeat;"></section>
+
+<?php
+endif;
