8834f05aeda93b9d99f32b18e6e445102d942bd8
Eric Stout <ericwstout@gmail.com>
2017-01-09 09:35:35 -0700

    Changes function to is_search

--- parts/standard-hero.php
+++ parts/standard-hero.php
@@ -7,7 +7,7 @@ elseif( is_home() ): ?>
 
   <section class="standard-hero" style="background: url(<?php bloginfo('template_url');?>/assets/img/News-press.jpg) center center no-repeat;"></section>
 
-<?php elseif( is_home() ): ?>
+<?php elseif( is_search() ): ?>
 
   <section class="standard-hero" style="background: url(<?php bloginfo('template_url');?>/assets/img/search-results.jpg) center center no-repeat;"></section>
 
