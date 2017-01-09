51c95721001a009ce76ae2efafafc635836e54f9
Eric Stout <ericwstout@gmail.com>
2017-01-09 09:26:05 -0700

    Adds news and press hero image

--- parts/standard-hero.php
+++ parts/standard-hero.php
@@ -2,7 +2,12 @@
 
 if( is_singular() && !has_post_thumbnail() ):
   // ssshhh
-else: ?>
+
+elseif( is_home() ): ?>
+
+  <section class="standard-hero" style="background: url(<?php bloginfo('template_url');?>/assets/img/News-press.jpg) center center no-repeat;"></section>
+
+<?php else: ?>
 
   <section class="standard-hero" style="background: url(<?php featuredURL('standard-hero');?>) center center no-repeat;"></section>
 
