d10fc722acc286f80bbaa767e59b6cb2f4aaa2bc
Eric Stout <ericwstout@gmail.com>
2017-01-25 15:25:27 -0700

    Adds scripts?

--- templates/big-upload.php
+++ templates/big-upload.php
@@ -2,6 +2,7 @@
 // Template Name: BigUpload
 get_header();
 
+echo file_get_contents('https://bigupload.oneilprint.com:446/scripts');
 echo file_get_contents('https://bigupload.oneilprint.com:446/');
 
 get_footer();
